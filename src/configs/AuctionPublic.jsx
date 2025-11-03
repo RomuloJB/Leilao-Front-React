efae34ceba22dd96f387f33270bd90dec342025b
RomuloJB <jordaobogoni@gmail.com>
2025-11-03 20:54:17 -0300

    add | explicações para começar o web socket

---
+++ src/configs/AuctionPublic.jsx
@@ -0,0 +1,7 @@
+/*só chama a requisição websocket se houver um leilao, se nao estará fazendo uma requisição atoa
+
+aqui neste arqiuvo tbm temos o envio da mensagem, o sendBid que envia um objeto json que o servidor salva um lance no banco e salva a resposta, retorna e atualiza a pagina do usuario
+
+
+*/
+
